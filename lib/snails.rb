090bfb810e019b5cc9651cd29411ed35f341d525
Cody Duval <cody.duval@gmail.com>
2014-12-02 15:42:16 -0500

    Basic error message on application errors

--- lib/snails.rb
+++ lib/snails.rb
@@ -4,9 +4,21 @@ require "snails/routing"
 module Snails
   class Application
     def call(env)
+      #temp hack to keep app from barfing when it
+      #can't find a favicon
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      end
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
-      text = controller.send(act)
+
+      begin
+        text = controller.send(act)
+      rescue
+        text = "That didn't work! Sorry"
+      end
+
       [200, {'Content-Type' => 'text/html'},
         [text]] 
     end
